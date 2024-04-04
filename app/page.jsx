6fb76f7c00e1c57673c17ed32d0780389aae1bc6
Jiawei Wu <robertwu1996@gmail.com>
2024-04-04 00:37:39 -0400

    implement search and click tag function

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,10 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI_Power Prompts</span>
+        <br />
+        <span className="orange_gradient text-center max-md:text-4xl">
+          AI_Power Prompts
+        </span>
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,10 +2,11 @@
 
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
+import Image from "next/image";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="mt-16 prompt_layout">
+    <div className="mt-2 prompt_layout">
       {data.map((post) => (
         <PromptCard
           key={post._id}
@@ -20,8 +21,8 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-
-  const handleSearchChange = (e) => {};
+  const [searchedPosts, setSearchedPosts] = useState([]);
+  const [isReset, setIsReset] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -30,9 +31,40 @@ const Feed = () => {
 
       setPosts(data);
     };
-
     fetchPosts();
-  }, []);
+  }, [isReset]);
+
+  const filterPrompts = (text) => {
+    const regex = new RegExp(text, "i");
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+
+    setTimeout(() => {
+      const result = filterPrompts(e.target.value);
+      setSearchedPosts(result);
+    }, 200);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") e.preventDefault();
+  };
+
+  const handleTagClick = (tag) => {
+    setPosts(() => posts.filter((item) => item.tag == tag));
+  };
+
+  const handleReset = () => {
+    setSearchText("");
+    setIsReset((prev) => !prev);
+  };
 
   return (
     <section className="feed">
@@ -44,9 +76,33 @@ const Feed = () => {
           onChange={handleSearchChange}
           required
           className="search_input peer"
+          onKeyDown={handleKeyDown}
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <Image
+        src="/assets/images/images.png"
+        width={40}
+        height={40}
+        className="rounded-full mt-4 cursor-pointer hover:shadow-xl"
+        alt="reset button"
+        onClick={handleReset}
+      />
+      {searchText.length !== 0 ? (
+        searchedPosts.length ? (
+          <PromptCardList
+            data={searchedPosts}
+            handleTagClick={handleTagClick}
+          />
+        ) : (
+          <p className="mt-10 text-center">
+            No prompt match you search
+            <br />
+            Start a new search
+          </p>
+        )
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
